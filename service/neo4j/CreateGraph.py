5c46b9ef9d80bbbcd7544bf54423ed40b7852545
Yu Hongchao <2871314919@qq.com>
2018-11-12 14:59:21 +0800

    实现了基本的展示功能 next step:   划过会出现信息面板

--- service/neo4j/CreateGraph.py
+++ service/neo4j/CreateGraph.py
@@ -3,6 +3,79 @@
 @Time:2018/11/8 17:06
 @Author:yuhongchao
 """
+import pandas as pd
+import os
+import cx_Oracle
+import numpy as np
 
-def create_graph(self):
-	pass
+
+def get_link():
+	filenames = os.listdir("../data/links")
+	res_lis = set()
+	res = np.array(['GF_NSRSBH', 'XF_NSRSBH', 'JE'])
+	for file in filenames:
+		str = "../data/links/" + file
+		temp = pd.read_pickle(str)
+		print(temp.columns)
+		res = np.vstack([res, temp])
+	pd.to_pickle(res, "links.pickle")
+
+
+def get_node():
+	filenames = os.listdir("../data/links")
+	res_lis = set()
+	for file in filenames:
+		str = "../data/links/" + file
+		temp = pd.read_pickle(str)
+		res_lis = res_lis | set(temp["GF_NSRSBH"])
+		res_lis = res_lis | set(temp["XF_NSRSBH"])
+	# pass
+	print(res_lis)
+
+	node_res = np.array(["Nsrsbh", "Nsrdzdah", "nsrmc", "Scjydz", "Fddbrmc"])
+	for i in res_lis:
+		mess = get_mess(i)
+		print(len(node_res))
+		if mess != None:
+			temp_arr = np.array([i, mess[3], mess[0], mess[1], mess[2]])
+			node_res = np.vstack([node_res, temp_arr])
+		else:
+			temp_arr = np.array([i, None, None, None, None])
+			node_res = np.vstack([node_res, temp_arr])
+
+	pd.to_pickle(node_res, "nodes.pickle")
+
+
+def get_mess(v):
+	sql = """
+		select dn.nsrmc,Dn.Scjydz,Dn.Fddbrmc,Dn.Nsrdzdah
+		from DJ_NSRXX dn
+		WHERE dn.nsrsbh ='{}'""".format(v)
+	# cur.executemany("""
+	#        select dn.NSRMC,ll.VERTEXID,FDDBRMC,HY_DM,Dn.Scjydz from (select * from  LG_GROUNDTRUTH) ll left
+	#        join DJ_NSRXX dn on ll.NSRDZDAH=dn.NSRDZDAH where ll.Vertexid=(:0)""", v_list)
+	print(sql)
+	cur.execute(sql)
+	res = cur.fetchall()
+	print(res)
+	return res[0] if len(res) >= 1 else None
+
+
+if __name__ == "__main__":
+	# 获得数据库的连接
+	con = cx_Oracle.connect('tax/taxgm2016@192.168.16.186/tax', encoding='utf8')
+	cur = con.cursor()
+
+	get_node()
+
+	# 释放数据库的连接
+	cur.close()
+	con.close()
+	# get_link()
+
+	# links = pd.read_pickle("links.pickle")
+	# l_pd = pd.DataFrame(links)
+	# print(l_pd)
+# nodes = pd.read_pickle("nodes.pickle")
+# l_pd = pd.DataFrame(nodes)
+# print(nodes)
--- service/neo4j/LinkToNeo4j.py
+++ service/neo4j/LinkToNeo4j.py
@@ -4,7 +4,11 @@
 @Author:yuhongchao
 """
 from neo4j.v1 import GraphDatabase
-from .CreateGraph import create_graph
+import pandas as pd
+import numpy as np
+
+# 将交易关系以及属性存储到neo4j数据库里面
+# 之后还要将嫌疑值存储在其中
 
 class LinkToNeo(object):
 
@@ -14,25 +18,43 @@ class LinkToNeo(object):
 	def close(self):
 		self._driver.close()
 
-	def print_greeting(self, message):
-		with self._driver.session() as session:
-			greeting = session.write_transaction(self._create_and_return_greeting, message)
-			print(greeting)
-
 	def create_graph(self):
 		with self._driver.session() as session:
-			session.write_transaction()
-			# print(greeting)
+			greeting = session.write_transaction(self._create_graph,self.get_info())
+			print(greeting)
 
 	@staticmethod
-	def _create_and_return_greeting(tx, message):
-		result = tx.run("CREATE (a:Greeting) "
-						"SET a.message = $message "
-						"RETURN a.message + ', from node ' + id(a)", message=message)
-		return result.single()[0]
+	def _create_graph(tx,mess):
+		tx.run(mess)
+
+	def get_info(self):
+		links = pd.read_pickle("basicData/links.pickle")
+		nodes = pd.read_pickle("basicData/nodes.pickle")
+		l_d = pd.DataFrame(links[1:, :], columns=["GF_NSRSBH", "XF_NSRSBH", "JE"])
+		n_d = pd.DataFrame(nodes[1:, :], columns=["NSRSBH", "Nsrdzdah", "nsrmc", "Scjydz", "Fddbrmc"])
+
+		n = np.array(n_d)
+		l = np.array(l_d)
+		res_str = ""
+		for i in range(1, n.__len__()):
+			a_tuple = n[i]
+			a_str = "CREATE ({}:Company {} nsrmc: '{}', nsrdzdah: '{}',nsrsbh:'{}', Scjydz: '{}', Fddbrmc:'{}'{})".format(
+				"n" + str(a_tuple[0]), "{", a_tuple[2], a_tuple[1], a_tuple[0], a_tuple[3], a_tuple[4], "}")
+			# /print(a_str)
+			res_str += a_str
+		for i in range(1, l.__len__()):
+			a_tuple = l[i]
+			# print(a_tuple)
+			a_str = "create  ({})-[:JIAOYI {}JE:{}{}]->({})".format(
+				"n" + str(a_tuple[0]), "{", round(a_tuple[2]), "}", "n" + str(a_tuple[1]))
+			print(a_str)
+			res_str += a_str
+		return res_str+";"
+
+
 
 if __name__ == "__main__":
 	# 连接neo4j
-	ltn = LinkToNeo("bolt://127.0.0.1:7687", "neo4j", "root")
-	ltn.print_greeting("你好啊")
-	# py_neo()
+	ltn = LinkToNeo("bolt://127.0.0.1:7687", "neo4j", "yuu6")
+	ltn.create_graph()
+# py_neo()
